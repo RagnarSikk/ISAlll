5f549968b5424c50b2c728d8153d29b04fb6644c
Raks <Raks@RAGNARSIKK3CFC>
2021-09-14 10:31:27 +0300

    Add project files.

---
+++ Data/PersonData.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data
+{
+    public class BaseData
+    {
+        public int Id { get; set; }
+    }
+    public class PersonRoleData : BaseData
+    {
+        public int PersonId { get; set; }
+    }
+    public class PersonData : BaseData
+    {
+    }
+    public class AthleteData : PersonRoleData
+    {
+    }
+    public class CoachData : PersonRoleData
+    {
+    }
+    public class NutritionistData : PersonRoleData
+    {
+    }
+    public class AdminstratorData : PersonRoleData
+    {
+    }
+    public class TrainingData : BaseData
+    {
+    }
+    public class ServicesData : BaseData
+    {
+    }
+}
---
+++ Domain/Person.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain
+{
+    public class Base
+    {
+    }
+    
+    public class PersonRole
+    {
+        public int PersonId { get; set; }
+    }
+    public class Person : Base
+    {
+    }
+    public class Athlete : PersonRole
+    {
+    }
+    public class Coach : PersonRole
+    {
+    }
+    public class Nutritionist : PersonRole
+    {
+    }
+    public class Adminstrator : PersonRole
+    {
+    }
+    public class Training : Base
+    {
+    }
+    public class Service : Base
+    {
+    }
+}
