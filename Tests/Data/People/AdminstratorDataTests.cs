581db2dc4fd08c49ec6f38a01f08d2e98eba566c
Ragnar Sikk <ragsik@ttu.ee>
2021-10-06 08:46:48 +0300

    Kaks ulesannet kodus lahendamiseks, mille eest punkte saab ainesse

--- Tests/Data/People/AdminstratorDataTests.cs
+++ Tests/Data/People/AdminstratorDataTests.cs
@@ -8,5 +8,20 @@ namespace isa3.Tests.Data.People
     public class AdminstratorDataTests : SealedTests<AdminstratorData, PersonRoleData>
     {
     }
+    //TODO: Gunnar andis video 103 lopus kaks taski, mille eest punkte koguda
+    //1) Testclass voiks nime katte saada klassinimest ehk midagi sellist > 
+    //[TestClass]
+    //public class AdminstratorDataTests : SealedTests<PersonRoleData>
+    //{  
+    //}
+    //2) Teha selline test for ex:
+    // [TestMethod] public void AdministartorIdTest() => isNullable<string>();
+    // ehk siis meetodi nimest saab katte AdministratorId, mille jargi otsib Admini klassist 
+    // ulesse selle id property, kontrollib et sellel propertyl oleks get; set; olemas
+    // proovib panna vaartuseks null, ja ka laseb omastada juhusliku stringi, molemad variandid 
+    // peaks olema testitud
+    //
+    // See peaks kaima siis iga Testklassi kohta? vist
+
 
 }
