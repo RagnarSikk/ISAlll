9f163c4297771d5fb25769f8b52a924460aa7a4b
Kristina Mere <krmere@ttu.ee>
2021-10-09 16:30:44 +0300

    IEntity and IRepo

---
+++ Domain/IRepo.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace isa3.Domain
+{
+    public interface IRepo<T>
+    {
+        Task<List<T>> Get();
+        Task<T> Get(int? id);
+        Task Delete(int? id);
+        Task Add(T obj);
+        Task Update(T obj);
+        T GetById(int? id);
+    }
+}
